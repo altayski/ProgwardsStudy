d2ae0fed0de594c09fef3cd312e08e44a88b36d8
sergeikirianov <altayski@ya.ru>
2023-03-09 22:56:54 +0100

    Homework №17

--- src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
+++ src/ru/progwards/java1/lessons/inheritance/ZonedTime.java
@@ -28,45 +28,32 @@ public class ZonedTime extends Time {
 
 
         if (this.getTimeZone() == null && time.getTimeZone() != null) {
-            timeCorrection = Math.abs(time.getTimeZone().hours * 60 * 60 + time.getTimeZone().minutes * 60);
+            timeCorrection = time.getTimeZone().hours * 3600 + time.getTimeZone().minutes * 60;
 
         } else if (this.getTimeZone() != null && time.getTimeZone() == null) {
-            timeCorrection = Math.abs(this.getTimeZone().hours * 60 * 60 + this.getTimeZone().minutes * 60);
+            timeCorrection = this.getTimeZone().hours * 3600 + this.getTimeZone().minutes * 60;
 
         } else if (this.getTimeZone() != null & time.getTimeZone() != null) {
-            timeCorrection = Math.abs(this.getTimeZone().hours * 3600 + this.getTimeZone().minutes * 60 + time.getTimeZone().hours * 3600 + time.getTimeZone().minutes * 60);
+            timeCorrection = this.getTimeZone().hours * 3600 + this.getTimeZone().minutes * 60 + time.getTimeZone().hours * 3600 + time.getTimeZone().minutes * 60;
         }
 
-        return timeTosec - timeCorrection;
+        return timeTosec + timeCorrection;
     }
 
     public static void main(String[] args) {
-        ZonedTime zt1 = new ZonedTime(4, 6, 44, new TimeZone(-2, 58));
-        Time t1 = new Time(21, 2, 7);
-        Time t2 = new Time(10, 2, 7);
-        System.out.println(t1.secondsBetween(t2));
+
+        ZonedTime zt1 = new ZonedTime(8, 40, 55, new TimeZone(0, 28));
+        Time t1 = new Time(18, 22, 23);
         System.out.println(zt1.secondsBetween(t1));
 
-//        ZonedTime zt1 = new ZonedTime(4, 51, 3, new TimeZone(-1));
-//        ZonedTime zt2 = new ZonedTime(9, 11, 44, new TimeZone(-2));
-//        System.out.println(zt1.secondsBetween(zt2));
 
+        ZonedTime zt2 = new ZonedTime(4, 6, 44, new TimeZone(-2, 58));
+        Time t2 = new Time(21, 2, 7);
 
-        TimeZone bratislava = new TimeZone(2);
-        TimeZone batumi = new TimeZone(4);
-        ZonedTime bratislavaTime = new ZonedTime(17, 14, 0, bratislava);
-        ZonedTime batumiTime = new ZonedTime(19, 14, 0, batumi);
-        ZonedTime london = new ZonedTime(15, 55, 0);
-        ZonedTime rekjavik = new ZonedTime(14, 55, 0);
-        System.out.println(london.secondsBetween(rekjavik));
-//        TimeZone RioZone = new TimeZone(-4, 0);
-//        ZonedTime rio = new ZonedTime(12, 10, 0, RioZone);
-//        System.out.println(london.secondsBetween(rio));
-//
-//        System.out.println(bratislavaTime.secondsBetween(london));//расчет разницы между гринвичем и братиславой
-//        System.out.println(bratislavaTime.secondsBetween(london));//расчет разницы между братиславой и гринвичем
-//
-        System.out.println(batumiTime.secondsBetween(bratislavaTime));
+        System.out.println(zt2.secondsBetween(t2));
 
+        ZonedTime zt3 = new ZonedTime(4, 51, 3, new TimeZone(-1));
+        ZonedTime zt4 = new ZonedTime(9, 11, 44, new TimeZone(-2));
+        System.out.println(zt3.secondsBetween(zt4));
     }
 }
